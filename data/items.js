2a80c0087d362b9a10c1959eb3019e0ca47eec3d
fpapado <fotis@futurice.com>
2018-01-21 18:44:21 +0200

    Initial setup done
    The lists work, the UI is more or less set. Stuff's cool, and
    choo is chugging along nicely.

---
+++ data/items.js
@@ -0,0 +1,12 @@
+const items = [
+  {name: "B-smokery", rating: 5},
+  {name: "Putte's", rating: 4},
+  {name: "Via Tribunali"},
+  {name: "Skiffer"},
+  {name: "Fornitaly"},
+  {name: "Piazza"},
+  {name: "...next?"},
+];
+
+module.exports = items;
+
--- views/main.js
+++ views/main.js
@@ -1,27 +1,84 @@
-var html = require('choo/html')
+var html = require('choo/html');
+var items = require('../data/items');
+import range from 'ramda/src/range';
 
-var TITLE = '🚂🚋🚋'
+var TITLE = '🚂🚋🚋';
 
-module.exports = view
+module.exports = view;
 
-function view (state, emit) {
-  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+function view(state, emit) {
+  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE);
 
   return html`
-    <body class="sans-serif">
-      <h1 class="f-headline pa3 pa4-ns">
-        Choo choo!
-      </h1>
+    <body class="sans-serif bg-washed-yellow near-black">
+      <div class="pa3 mw7-ns center">
+        <div class="pv3-ns mt3 mb4">
+          <h1 class="ma0 f-headline-ns f1 tracked lh-title ttu">
+            Nom Nom Nomicon
+          </h1>
+        </div>
 
-      <div class="ph3 ph4-ns">
-        <p>Current number of clicks: ${state.totalClicks}</p>
+        ${viewItems(items)}
 
-        <button class="f5 dim br-pill ph3 pv2 mb2 dib white bg-hot-pink bn pointer" onclick=${handleClick}>Click Me!</button>
+        ${viewFooter()}
       </div>
     </body>
-  `
+  `;
 
-  function handleClick () {
-    emit('clicks:add', 1)
+  function handleClick() {
+    emit('clicks:add', 1);
   }
+}
+
+function viewItems(items) {
+  return html`
+    <div class="">
+      <ul class="list mv4 pl0 measure f4 f3-ns">
+        ${items.map(viewItem)}
+      </ul>
+    </div>
+  `;
+}
+
+function viewItem(item) {
+  return html`
+    <li class="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30 flex justify-between items-center">
+      ${item.name}
+      ${viewRating(item.rating)}
+    </li>
+  `;
+}
+
+function viewRating(rating) {
+  return !!rating
+    ? html`
+    <div>
+      ${range(0, 5).map(i => (i < rating ? '+' : '-'))}
+    </div>
+  `
+    : html`<p className="mv0 f6 silver">not yet visited</p>`;
+}
+
+function viewFooter() {
+  return html`
+  <div class="pt4 pr2 pb3 flex items-center justify-between">
+    <p class="mw5 mw7-ns f6 lh-copy">
+      Made with 🍕 by <span class="b">Fotis Papadogeorgopoulos</span>
+    </p>
+
+    <div class="grow">
+        ${viewContribution()}
+    </div>
+  </div>
+  `;
+}
+
+function viewContribution() {
+  return html`
+    <a href="https://github.com/fpapado/nomnom/data/places/js" class="link near-black hover-light-red">
+      <p class="w4 pa3 ma0 f6 fw6 lh-copy ba bw1 b--near-black" style="transform: rotate3d(0, 0, 1, 10deg)">
+          Have a suggestion?
+      </p>
+    </a>
+  `;
 }
