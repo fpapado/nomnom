d244cde13e22ca8d1bcbcd9d5194dbed10b0ef88
fpapado <fotis@futurice.com>
2018-01-24 13:11:49 +0200

    Add Alfons'

--- data/places.js
+++ data/places.js
@@ -1,6 +1,7 @@
 const places = [
   {name: "B-smokery", rating: 5},
   {name: "Putte's", rating: 4},
+  {name: "Alfons'", rating: 4},
   {name: "Via Tribunali"},
   {name: "Skiffer"},
   {name: "Fornitaly"},
