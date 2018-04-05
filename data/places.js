1d90cfeed927a090f51ac3bb0384d664f24262dd
fpapado <fotis@futurice.com>
2018-04-05 18:42:19 +0300

    Update list

--- data/places.js
+++ data/places.js
@@ -1,13 +1,12 @@
 const places = [
+  {name: "Treffipub", rating: 5},
+  {name: "Skiffer", rating: 4},
+  {name: "Roslund", rating: 4},
   {name: "B-smokery", rating: 5},
   {name: "Giwa", rating: 4},
   {name: "Putte's", rating: 4},
   {name: "Alfons'", rating: 4},
-  {name: "Via Tribunali"},
-  {name: "Skiffer"},
   {name: "Fornitaly"},
-  {name: "Pjazza"},
-  {name: "Roslund"},
 ];
 
 module.exports = places;
